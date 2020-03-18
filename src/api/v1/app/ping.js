2b52aa3524c646dcbd4efb4c41f9265860737e7b
Wietse Wind <mail@wietse.com>
2020-03-18 15:57:21 +0100

    Store appVersion && appLanguage

--- src/api/v1/app/ping.js
+++ src/api/v1/app/ping.js
@@ -1,6 +1,29 @@
 const getBadgeCount = require('@api/v1/internal/get-user-badge-count')
 
 module.exports = async (req, res) => {
+  if (typeof req.body === 'object' && req.body !== null) {
+    if (typeof req.body.appLanguage !== 'undefined' || typeof req.body.appVersion !== 'undefined') {
+      await req.db(`
+        UPDATE
+          devices
+        SET
+          device_appVersion = :appVersion,
+          device_appLanguage = :appLanguage
+        WHERE
+          device_uuidv4_bin = UNHEX(REPLACE(:deviceUuid,'-',''))
+        AND
+          (device_disabled IS NULL OR device_disabled > NOW())
+        AND
+          device_accesstoken_bin IS NOT NULL
+        LIMIT 1
+      `, { 
+        appVersion: req.body.appVersion || null,
+        appLanguage: req.body.appLanguage || null,
+        deviceUuid: req.__auth.device.uuidv4
+      })
+    }
+  }
+
   res.json({ 
     pong: true,
     tosAndPrivacyPolicyVersion: Number(req.config.TosAndPrivacyPolicyVersion) || 0,
