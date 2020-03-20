e230de4237699c35429fac5561d1c9e37191b387
Wietse Wind <mail@wietse.com>
2020-03-20 12:28:18 +0100

    Add IT translation, thanks @thexrpdude-it

---
+++ translations/it.js
@@ -0,0 +1,61 @@
+module.exports = {
+  LANGUAGE_CODE: 'it',
+  ERROR: `Errore`,
+  ERROR_404_BODY: `Spiacente, la pagina richiesta non è stata trovata.`,
+  ERROR_500_BODY: `Spiacente, la tua richiesta non può essere processata in questo momento.`,
+  LIPSUM: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin maximus sem ante, sit amet semper mauris venenatis non. Fusce posuere, libero ut volutpat ornare, massa ipsum hendrerit ipsum, sed finibus ipsum augue a nisl. Etiam at tempus ante, sit amet tristique leo. Vestibulum lobortis nisl a volutpat rhoncus. Ut facilisis ante a iaculis sollicitudin. Ut consectetur magna at libero vulputate, id pulvinar dui rutrum. Etiam facilisis finibus tincidunt.`,
+  BY_NAME: `XPRL Labs`,
+  XRPL_SIGNPLATFORM: `Piattaforma per la firma XRPL (BETA)`,
+  TITLE_SIGNTX: `Richiesta di firma`,
+  ALREADY_SIGNED: `Già processata`,
+  PAYLOAD_RESOLVED: `Questa richiesta è già stata processata (firmata o rifiutata).`,
+  SCAN_WITH_APP: `Scansiona questo QR con <b class="font-logo">xumm</b>`,
+  PAYLOAD_NOT_FOUND: `Richiesta di firma non trovata.`,
+  PAYLOAD_EXPIRED: `Richiesta di firma scaduta.`,
+  PAYLOAD_BACKEND_ERROR: `Spiacente, si è verificato un errore durante il recupero dei dati da firmare.`,
+  PROFILE_BACKEND_ERROR: `Spiacente, si è verificato un errore durante il recupero del profilo utente.`,
+  PAYLOAD_PAGE_FOR_MOBILE: `Per firmare una richiesta, assicurati di avere l'app <b class="font-logo">xumm</b> installata.`,
+  APPLICATION_DISABLED: `L'applicazione <q><b>__appname__</b></q> è disabilitata.`,
+  WAIT_CONNECT_PLATFORM: `Connessione alla Piattaforma di Firma.`,
+  WAIT_OPEN: `In attesa dell'apertura di una  
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  richiesta verso l'app XUMM.`,
+  WAIT_SCAN_QR: `In attesa che tu effettui la scansione della 
+                  <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                  richiesta tramite l'app XUMM.`,
+  WAIT_SCAN_QR_DETAILED: `In attesa che tu effettui la scansione ed apra 
+                            <code class="text-primary"><b class="text-primary">__txtype__</b></code>
+                          a 
+                            <code class="text-primary"><b>__txdestination__</b></code>
+                          attraverso l'app XUMM.`,
+  WAIT_FOR_RESOLVE: `In attesa che tu risolva (accetti / rifiuti) la  
+                      <code class="text-primary"><b class="text-primary"><q>__txtype__</q></b></code>
+                      richiesta nell'app XUMM.`,
+  SHOW_TRANSACTION: `Mostra richiesta di firma`,
+  RESOLVED: `La richiesta è stata firmata correttamente.`,
+  RESOLVED_NEXT_RETURN: `Sarai riportato a <q><b>__appname__</b></q> entro pochi secondi`,
+  REJECTED: `La richiesta di firma è stata rifiutata.`,
+  REJECTED_NEXT_RETURN: `Sarai riportato a <q><b>__appname__</b></q> entro pochi secondi`,
+  GET_APP_IOS_TITLE: `Scarica 𝘅𝘂𝗺𝗺 per iOS su App Store`,
+  GET_APP_ANDROID_TITLE: `Scarica 𝘅𝘂𝗺𝗺 per Android su Google Play`,
+  MODAL_CLOSE: `Chiudi`,
+  GET_APP_IOS_MODAL_TITLE: `Scarica <b class="font-logo">xumm</b> per iOS`,
+  GET_APP_ANDROID_MODAL_TITLE: `Scarica <b class="font-logo">xumm</b> per Android`,
+  GET_APP_IOS_MODAL_SCAN: `Scansiona il QR con il tuo dispositivo iOS per visitare <i class="fab fa-apple"></i> l'App Store.`,
+  GET_APP_ANDROID_MODAL_SCAN: `Scansiona il QR con il tuo dispositivo Android per visitare il <i class="fab fa-google-play"></i> Google Play Store.`,
+  GET_APP_IOS_MODAL_VISIT: `Visita l'App Store Apple`,
+  GET_APP_ANDROID_MODAL_VISIT: `Visita il Google Play Store`,
+  PAYLOAD_PUSH_NOTIFICATION_SENT: `Abbiamo inviato una notifica push sul tuo dispositivo(i) mobile.`,
+  DIDNT_RECEIVE_PUSH_TEXT: `Non hai ricevuto la notifica?`,
+  SHOW_QR: `Scansiona il QR code in alternativa`,
+  OPEN_IN_XUMM: 'Open in the <b class="font-logo">xumm</b> App',
+  PLEASE_INSTALL_APP: `Per favore installa <b class="font-logo">xumm</b>`,
+  DASH_OR: '-or-',
+  INSTALL_APP: `Installa <b class="font-logo">xumm</b>`,
+  TITLE_TX_SCANNED: `Aperto in <b class="font-logo">xumm</b>`,
+  PAYLOAD_ALREADY_OPENED: `La richiesta di firma è stata aperta`,
+  NEED_QR_PAYLOAD_QR: `Puoi riaprire la richiesta di firma. La richiesta scade entro <span class="ts-moment">__in_x_time_expire__</span>.`,
+  PUSH_MSG_SIGN_REQUEST: `Richiesta di firma`,
+  PUSH_MSG_TXTYPE_TO_DEST: `__type__ a __destination__`,
+  PUSH_MSG_SIGNIN_REQ: `Accedi a  __appname__`
+}
