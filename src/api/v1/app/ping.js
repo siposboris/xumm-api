4ec0a0b0fabd867c99625ec31c0c4216a3e62390
Wietse Wind <mail@wietse.com>
2020-03-27 11:58:00 +0100

    Fix PING, longer version string & improved updating

--- src/api/v1/app/ping.js
+++ src/api/v1/app/ping.js
@@ -1,26 +1,35 @@
+const log = require('debug')('app:ping')
 const getBadgeCount = require('@api/v1/internal/get-user-badge-count')
 
 module.exports = async (req, res) => {
   if (typeof req.body === 'object' && req.body !== null) {
     if (typeof req.body.appLanguage !== 'undefined' || typeof req.body.appVersion !== 'undefined') {
-      await req.db(`
-        UPDATE
-          devices
-        SET
-          device_appVersion = :appVersion,
-          device_appLanguage = :appLanguage
-        WHERE
-          device_uuidv4_bin = UNHEX(REPLACE(:deviceUuid,'-',''))
-        AND
-          (device_disabled IS NULL OR device_disabled > NOW())
-        AND
-          device_accesstoken_bin IS NOT NULL
-        LIMIT 1
-      `, { 
-        appVersion: req.body.appVersion || null,
-        appLanguage: req.body.appLanguage || null,
-        deviceUuid: req.__auth.device.uuidv4
-      })
+      try {
+        await req.db(`
+          UPDATE
+            devices
+          SET
+            device_appVersion = IF(:appVersion IS NOT NULL, :appVersion, device_appVersion),
+            device_appLanguage = IF(:appLanguage IS NOT NULL, :appLanguage, device_appLanguage)
+          WHERE
+            device_uuidv4_bin = UNHEX(REPLACE(:deviceUuid,'-',''))
+          AND
+            (device_disabled IS NULL OR device_disabled > NOW())
+          AND
+            device_accesstoken_bin IS NOT NULL
+          LIMIT 1
+        `, { 
+          appVersion: typeof req.body.appVersion !== 'undefined'
+            ? req.body.appVersion.slice(0, 24)
+            : null,
+          appLanguage: typeof req.body.appVersion !== 'undefined'
+            ? req.body.appLanguage.slice(0, 12)
+            : null,
+          deviceUuid: req.__auth.device.uuidv4
+        })
+      } catch (e) {
+        log(e.message, req.body)
+      }
     }
   }
 
